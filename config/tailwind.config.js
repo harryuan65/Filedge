4529e12b849d074784bde9f60616362379a91ca9
harryuan65 <harryuan.65@gmail.com>
2023-02-28 21:33:58 +0800

    feat: add custom tailwind config, disable eslint for now

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        accent: '#2AC0CF',
+        black: '#222',
+        border: '#c2c2c2',
+        block: '#FBFBFB',
       },
     },
   },
@@ -18,5 +24,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
-}
+  ],
+};

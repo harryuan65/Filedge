8a283b04f504538cd3421aa8d734bd0bf0ef3d03
harryuan65 <harryuan.65@gmail.com>
2023-02-28 22:38:34 +0800

    feat: disable new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "user_files#index"
-  resources :user_files do
+  resources :user_files, except: %i[new] do
     resources :sharing_links, shallow: true, path: "share"
   end
   devise_for :users, controllers: {

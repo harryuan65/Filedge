d8ab423f9e212f4ba3c36266eb1f13b65cc50ad8
harryuan65 <harryuan.65@gmail.com>
2023-03-01 22:18:52 +0800

    test(view): user_files

--- spec/views/user_files/index.html.tailwindcss_spec.rb
+++ spec/views/user_files/index.html.tailwindcss_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "user_files/index" do
+  let(:user) { create(:user) }
+  let!(:files) { create_list(:user_file, 3, user: user) }
+
   before do
-    assign(:user_files, [
-      UserFile.create!,
-      UserFile.create!
-    ])
+    assign(:user_file, user.files.new)
+    assign(:user_files, files)
   end
 
   it "renders a list of user_files" do
     render
-    expect(rendered).to contain("Show this user file")
+    regex = Regexp.union(files.map { |file| dom_id(file) })
+    expect(rendered).to match(regex)
   end
 end
--- spec/views/user_files/show.html.tailwindcss_spec.rb
+++ spec/views/user_files/show.html.tailwindcss_spec.rb
@@ -1,12 +1,21 @@
 require "rails_helper"
 
+# rubocop:disable RSpec/ExampleLength
 RSpec.describe "user_files/show" do
+  let(:user) { create(:user) }
+  let(:file) { create(:user_file, user: user) }
+
   before do
-    @user_file = assign(:user_file, UserFile.create!)
+    assign(:user_file, file)
   end
 
-  it "renders attributes in <p>" do
+  it "renders the file information" do
     render
-    expect(rendered).to contain("Back to user_files")
+    assert_select "tbody tr" do
+      assert_select "td", file.file_size.to_s
+      assert_select "td", file.created_at.strftime("%b %d, %Y")
+      assert_select "td form:match('action', ?)", Regexp.new("/user_files/#{file.id}")
+    end
   end
 end
+# rubocop:enable RSpec/ExampleLength

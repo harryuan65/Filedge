5ac4dd6e1d5e99748c764136f5f795b260788c89
harryuan65 <harryuan.65@gmail.com>
2023-03-03 21:40:20 +0800

    fix: run tailwind watcher in docker dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,8 @@ rm -rf tmp
 set -e
 
 if [[ $1 == "start_service" ]]; then
-  if [[ $PORT ]]; then
-    bundle exec rails s -b 0.0.0.0 -p $PORT
-  else
-    bundle exec rails s -b 0.0.0.0 -p 3000
-  fi
+  bin/rails tailwindcss:watch &
+  bundle exec rails s -b 0.0.0.0 -p 3000
 else
   exec "$@"
 fi
